34de8c6a5a26791ce98e76a255b9163103ba2b41
vilem.raska <vilem.raska@kiwi.com>
2022-06-11 07:10:00 +0200

    Initial Commit

---
+++ Shared/MinitaskerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinitaskerApp.swift
+//  Shared
+//
+//  Created by Vilém Raška on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MinitaskerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
